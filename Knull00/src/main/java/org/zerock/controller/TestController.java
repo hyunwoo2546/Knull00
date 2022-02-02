a9a6c588118d40db406f132174e044aa3dfeedf2
Hyunwoo <hyunwoo2546@naver.com>
2022-02-02 23:51:46 +0900

    설날 마지막 연휴

--- Knull00/src/main/java/org/zerock/controller/TestController.java
+++ Knull00/src/main/java/org/zerock/controller/TestController.java
@@ -1,12 +1,21 @@
 package org.zerock.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.SampleVO;
+import org.zerock.domain.Ticket;
 
 import lombok.extern.log4j.Log4j;
 
@@ -15,11 +24,25 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class TestController {
 
-	@GetMapping(value = "/getText")
-	public List<SampleVO> getTest() {
-
-		return IntStream.range(1, 10).mapToObj(i -> new SampleVO(i, i + "First", i + "Last")).collect(Collectors.toList());
+	/*
+	 * @GetMapping("/getTest/{cat}/{pid}") public String[] getTest(
+	 * 
+	 * @PathVariable("cat") String cat,
+	 * 
+	 * @PathVariable("pid") Integer pid) {
+	 * 
+	 * return new String[] { "catagori" + cat, "productid" + pid };
+	 * 
+	 * }
+	 */
+	
+	@PostMapping("/ticket")
+	public Ticket convert(@RequestBody Ticket ticket) {
 		
-	}
+		log.info("convert........" + ticket);
+		
+		return ticket;
+		
+	} 
 	
 }
--- Knull00/src/main/java/org/zerock/domain/Ticket.java
+++ Knull00/src/main/java/org/zerock/domain/Ticket.java
@@ -1,11 +1,9 @@
 package org.zerock.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-public class SampleVO {
+public class Ticket {
 
 	private int tno;
 	private String owner;

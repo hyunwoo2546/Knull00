f5477cb875d9ca5a35ce81588584f5552a997dce
Hyunwoo <hyunwoo2546@naver.com>
2022-02-03 17:41:53 +0900

    2/3

--- Knull00/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ Knull00/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,10 +1,13 @@
 package org.zerock.mapper;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -14,13 +17,24 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ReplyMapperTests {
 	
+	private Long[] bnoArr = {655410L,655409L,655408L,655407L,655406L};
+	
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
 	@Test
 	public void testMapper() {
-		
-		log.info(mapper);
+
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			
+			ReplyVO vo = new ReplyVO();
+			
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글 테스트" + i);
+			vo.setReplyer("댓글 작성자" + i);
+			
+			mapper.insert(vo);
+		});
 		
 	}
 

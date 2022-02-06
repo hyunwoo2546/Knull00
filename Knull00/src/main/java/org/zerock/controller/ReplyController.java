f168b4661c95f2fff6b103210a65c9e7e06c2375
Hyunwoo <hyunwoo2546@naver.com>
2022-02-06 19:34:17 +0900

    2/5

--- Knull00/src/main/java/org/zerock/controller/ReplyController.java
+++ Knull00/src/main/java/org/zerock/controller/ReplyController.java
@@ -2,12 +2,20 @@ package org.zerock.controller;
 
 
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
@@ -22,6 +30,7 @@ public class ReplyController {
 
 	private ReplyService service;
 	
+	/* # 댓글 등록 */
 	@PostMapping(value="/new",
 			consumes = "application/json",
 			produces = {MediaType.TEXT_PLAIN_VALUE})
@@ -35,6 +44,68 @@ public class ReplyController {
 		return insertCount == 1
 			   ? new ResponseEntity<>("success",HttpStatus.OK)
 			   : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	/* # 특정 게시물 댓글 목록 조회 */
+	@GetMapping(value = "/pages/{bno}/{page}",
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable("page") int page,
+			@PathVariable("bno") Long bno) {
+
+		log.info("get List....");
+		
+		Criteria cri = new Criteria(page,10);
+		
+		log.info(cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno),HttpStatus.OK);
+	}
+	
+	/* # 댓글 조회 */
+	@GetMapping(value = "/{rno}",
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+	
+		log.info("댓글 조회...." + rno);
+		
+		return new ResponseEntity<>(service.get(rno),HttpStatus.OK);
+	}
+	
+	/* # 댓글 삭제 */
+	@DeleteMapping(value = "/{rno}" ,
+			produces = {
+					MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+
+		log.info("삭제 처리...." + rno );
+		
+		return service.remove(rno) == 1
+				? new ResponseEntity<>("success",HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	/* # 댓글 수정 */
+	@RequestMapping(method = {RequestMethod.PUT,RequestMethod.PATCH},
+			value = "/{rno}",
+			consumes = "application/json",
+			produces = {
+					MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno) {
+	
+		vo.setRno(rno);
+		
+		log.info("rno : " + rno);
+		
+		log.info("댓글 수정...." + vo);
+		
+		return service.modify(vo) == 1
+				? new ResponseEntity<>("success",HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
